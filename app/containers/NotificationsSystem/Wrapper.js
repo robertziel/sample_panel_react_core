9385d45f5aea3258e6a14c4be849b84f12bb222c
Robert Zieliński <hello@robertz.co>
2019-09-24 13:30:41 +0900

    Add notifications wrapper styles

---
+++ app/containers/NotificationsSystem/Wrapper.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  .notification-item {
+    width: 400px;
+
+    .notification-title {
+      text-transform: uppercase;
+    }
+  }
+`;
+
+export default Wrapper;
--- app/containers/NotificationsSystem/index.js
+++ app/containers/NotificationsSystem/index.js
@@ -4,6 +4,12 @@ import NotificationSystem from 'react-notifications-component';
 
 import 'react-notifications-component/dist/theme.css';
 
+import Wrapper from './Wrapper';
+
 export default function NotificationsSystem() {
-  return <NotificationSystem />;
+  return (
+    <Wrapper>
+      <NotificationSystem />
+    </Wrapper>
+  );
 }
